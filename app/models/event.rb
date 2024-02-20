9bc9627b6103566537f4e49e45d090e49ea22001
bmanquen <bmanquen1993@gmail.com>
2024-02-20 04:42:55 -0600

    Changed location to street_address and added city, state, and zip

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,7 @@
 class Event < ApplicationRecord
   enum :childcare, [:kid_friendly, :childcare_options]
 
-  validates :title, presence: true
-  validates :summary, presence: true
-  validates :date, comparison: {greater_than: -> { Date.today }}, allow_nil: true
-  validates :cost, comparison: {greater_than: 0}, allow_nil: true
+  validates :title, :summary, presence: true
+  validates :date, comparison: { greater_than: Date.today }, allow_nil: true
+  validates :cost, comparison: { greater_than: 0 }, allow_nil: true
 end

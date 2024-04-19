4e64cbd37b2860fc65417535c87066cd8be974b1
Brenden Manquen <brendenmanquen@Brookes-MacBook-Air.local>
2024-04-19 14:26:47 -0500

    Added upload-file endpoint for S3

--- app/controllers/upload_file_controller.rb
+++ app/controllers/upload_file_controller.rb
@@ -6,6 +6,7 @@ class UploadFileController < ApplicationController
     bucket_name = Rails.env == 'production' ? 'momfit' : 'momfit-dev'
     obj = s3.bucket(bucket_name).object(params[:file].original_filename)
     obj.upload_file(params[:file].tempfile, acl: 'public-read')
-    render json: {message: 'File uploaded successfully'}, status: :ok
+    image_url = obj.public_url.to_s
+    render json: {message: 'File uploaded successfully', imageUrl: image_url}, status: :ok
   end
 end
